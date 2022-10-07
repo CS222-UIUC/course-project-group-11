225842d6f240dc980c7f61db371b64339ade6100
Yoda, Takashi <yodatakashi11@gmail.com>
2022-10-07 17:08:56 -0500

    PLEASE READ THE FIRST PART OF README.md

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# Group 11 project (CS 222)
+# ClassKonnect (Group 11 project in CS 222)
 
 ### [our proposal](https://docs.google.com/document/d/1UTmIv_weaekLc5lBQdIZ6UTJ0D-l6-Cf1y4TpaT1hT0/edit)
+
+# Update on Oct 7
+For now, we do not use SQL databases because we have not agreed on how to host the databases (if we are allowed to set up and interact with databases locally, never in a production environment, we will take that route). Thus, as global variables, we put some random user information and course information. In the log-in page, hence, put 'guest' as our NetID and '1234' as our password.
+
+Update:
+I managed to extract info on every section of every course offered at UIUC in Fall 2022 from Course Explorer API. Refer to `course_extractor.ipynb`. If you run each cell in the jupyter notebook, you will obtain `fall2022_courses.csv` which is exactly what we need. Now that it is just a few steps away from setting up a database, I guess each of us can set it up locally. I consider setting it up on AWS so that we all can interact with the DB.
+
+Also, when you click on the course button (CS222 SL1 specifically), sample course/section information on CS222 SL1 will show up. For other courses, it won't work at this time.
 
 ## Requirements
 Refer to the `requirements.txt` file.
@@ -14,8 +22,6 @@ pip install -r requirements.txt
 flask run
 ```
 
-For now, we do not use SQL databases because we have not agreed on how to host the databases (if we are allowed to set up and interact with databases locally, never in a production environment, we will take that route). Thus, as global variables, we put some random user information and course information. In the log-in page, hence, put 'guest' as our NetID and '1234' as our password.
-
 ## For the frontend developers
 Feel free to edit the HTML/CSS files in the `static` and `templates` folders.
 `base.html` serves as a template for the `main.html` and other html files created in the future.
@@ -25,4 +31,4 @@ Feel free to edit the HTML/CSS files in the `static` and `templates` folders.
 ## Test Coverage
 We are still having difficulty in using pytest for testing a flask web app, but here's the result of running the following command: `pytest --cov --cov-branch -v test.py`
 
-<img src="/static/img/test_cov.png" alt="test_coverage" style="height: 250px; width:2276px;"/>
+<img src="/static/img/test_cov_week1.png" alt="test_coverage" style="height: 250px; width:2276px;"/>
--- app.py
+++ app.py
@@ -5,19 +5,39 @@ app = Flask(__name__)
 
 # for the time being, we will hold a piece of data for development here and later delete
 # and replace it with actual data from our database
+# pylint: disable=line-too-long
 
-# netid and pw
-login_info = {"guest": "1234",
-              "XXX": "0000",
-              "YYY": "0000",
-              "ZZZ": "0000"}
-# user_name -> list of courses he or she is taking
-user_info = {"guest": {"courses": ["CS225", "CS222", "CS233", "MATH415", "STAT400"]},
-             "XXX": {"courses": ["CS374", "CS440", "CS341", "CS498"]},
-             "YYY": {"courses": ["CS128", "MATH231", "CS173"]},
-             "ZZZ": {"courses": ["CS225", "CS222", "CS233", "STAT410"]}}
-
-
+# user information
+users = {'guest': {'pw': '1234',
+                   'discId': 'guest#1234',
+                   'courses': [("CS225", '', ''), ("CS222", '74484', 'SL1'), ("CS233", '', ''), ("MATH415", '', ''), ("STAT400", '', '')]},
+         'XXX': {'pw': '0000',
+                 'discId': '',
+                 'courses': [("CS374", '', ''), ("CS440", '', ''), ("CS341", '', ''), ("CS498", '', '')]},
+         'YYY': {'pw': '0000',
+                 'discId': '',
+                 'courses': [("CS128", '', ''), ("MATH231", '', ''), ("CS173", '', '')]},
+         'ZZZ': {'pw': '0000',
+                 'discId': '',
+                 'courses': [("CS225", '', ''), ("CS222", '74484', 'SL1'), ("CS233", '', ''), ("STAT410", '', '')]}}
+# sample section information to display course information
+sections = [{'year': '2022',
+            'semester': 'fall',
+            'subjectName': 'Computer Science',
+            'subjectId': 'CS',
+            'courseNum': '222',
+            'courseName': 'Software Design Lab',
+            'description': "Design and implementation of novel software solutions. Problem identification and definition; idea generation and evaluation; and software implementation, testing, and deployment. Emphasizes software development best practices—including framework selection, code review, documentation, appropriate library usage, project management, continuous integration and testing, and teamwork. Prerequisite: CS 128; credit or concurrent registration in CS 225. Restricted to majors in Computer Science undergraduate curricula only.",
+            'creditHours': '1 hours',
+            'CRN': '74484',
+            'sectionNum': 'SL1',
+            'sectionType': 'Lecture-Discussion',
+            'startTime': '02:00 PM',
+            'endTime': '02:50 PM',
+            'daysOfTheWeek': 'F',
+            'roomNum': '0027/1025',
+            'buildingName': 'Campus Instructional Facility',
+            'mainInstructor': "Woodley, M"}]
 
 @app.route('/')
 def index():
@@ -28,48 +48,41 @@ def index():
 def user_login():
     """Route for /main"""
     items = request.form
-    if items["netid"] in login_info:
-        if items["pw"] == login_info[items["netid"]]:
+    if items["netid"] in users:
+        if items["pw"] == users[items["netid"]]['pw']:
             # successfully logged in
             required_info = []
             # here, we're supposed to manipulate data, writing queries and so on
             # but due to the issue where we don't have any solid idea as to
             # how to host a web/SQL server
             required_info.append(items["netid"])
-            required_info.append(user_info[items["netid"]])
-            required_info.append(list(login_info.keys())[1:])
+            required_info.append(users[items["netid"]]['courses'])
+            required_info.append(list(users.keys())[1:])
             return render_template("main.html", items=required_info), 200
     # did not successfully logged in (have to do something with this)
-    return "", 404
-
-@app.route('/temp', methods=["GET"])
-def temp():
-    """This route is still under construction and
-    shows almost nothing but later will be developed"""
-    return render_template("base.html"), 200
+    return render_template("error.html"), 404
 
 @app.route('/register', methods=["POST"])
 def register():
-    """This route is still under construction and
-    shows almost nothing but later will be dev"""
-    # return "reached", 200
+    """Route for /register"""
     return render_template("register.html"), 200
 
-# @app.route('/lookup/<course>', methods=["GET"])
-# def courses(course):
-#     """Route for /lookup"""
-#     fake_enrollments = ["CS225", "CS222", "MATH415", "CS233", "STAT400"]
-#     if course in fake_enrollments:
-#         return '', 200
-#     return '', 404
+@app.route('/user_info', methods=["POST"])
+def user_info():
+    """ Route for /user_info
+        This is supposed to show user information but
+        is under development at this moment.
+    """
+    return render_template("base.html"), 200
 
-# @app.route('/enrollment', methods=["POST"])
-# def enrollment():
-#     """Route for /enrollment"""
-#     requested_course = request.form['subject'] + request.form['number']
-#     server_url = os.getenv('BASE_URL')
-#     req = requests.get(f'{server_url}/lookup/{requested_course}')
-#     if req.status_code == 200:
-#         found = "We confirmed that you're enrolled in " + requested_course + "!"
-#         return found, 200
-#     return '', 404
+@app.route('/section/<course>/<section>', methods=["POST"])
+def section_info(course, section):
+    """Route for /section"""
+    # look up a section info
+    for sec in sections:
+        course_num = sec['subjectId'] + sec['courseNum']
+        if course_num == course and sec['sectionNum'] == section:
+            items = sec
+            return render_template("section.html", items=items), 200
+    # section info not found (have to do something with this)
+    return '', 404
