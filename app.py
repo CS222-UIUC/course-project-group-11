7e638aa8608848fb98dff0202928892bfc5e1382
Yoda, Takashi <yodatakashi11@gmail.com>
2022-10-21 23:22:07 -0500

    before making PR

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password' # password should be the pw for your local SQL server
+app.config['MYSQL_HOST'] = 'classkonnect.ckulwu8pg879.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'takashi_yd'
+app.config['MYSQL_PASSWORD'] = 'cU5SbJW6BCoLHmcyXe7n'
 app.config['MYSQL_DB'] = 'classkonnect'
 mysql = MySQL(app)
 
@@ -18,16 +18,7 @@ mysql = MySQL(app)
 #             ('yoda2', 74484), ('yoda2', 35917), ('yoda2', 63733), ('yoda2', 35030)
 #             ('guest', 74484), ('guest', 35917), ('guest', 59328), ('guest', 63037)
 #             ('inst', 74484), ('inst', 77096), ('inst', 66445), ('inst', 35030)
-# courses (subjectID, courseID, courseName, CRN)
-#         ('STAT', 400, 'Statistics and Probability I', 35030)
-#         ('CS', 225, 'Data Structures', 35917)
-#         ('MATH', 241, 'Calculus III', 59328)
-#         ('MATH', 415, 'Applied Linear Algebra', 63037)
-#         ('CS', 233, 'Computer Architecture', 63733)
-#         ('CS', 361, 'Probability & Statistics for Computer Science', 66298)
-#         ('CS', 374, 'Introduction to Algorithms & Models of Computation', 66445)
-#         ('CS', 222, 'Software Design Lab', 74484)
-#         ('CS', 340, 'Introduction to Computer Systems', 77096)
+# courses: now this table contains the exact same elements as the csv file
 
 # pylint: disable=line-too-long
 @app.route('/')
@@ -39,32 +30,28 @@ def index():
 def user_login():
     """Route for /main"""
     items = request.form
-    # if items["netid"] in users:
-    #     if items["pw"] == users[items["netid"]]['pw']:
-    #         # successfully logged in
-    #         required_info = []
-    #         # here, we're supposed to manipulate data, writing queries and so on
-    #         # but due to the issue where we don't have any solid idea as to
-    #         # how to host a web/SQL server
-    #         required_info.append(items["netid"])
-    #         required_info.append(users[items["netid"]]['courses'])
-    #         required_info.append(list(users.keys())[1:])
-    #         return render_template("main.html", items=required_info), 200
-    # # did not successfully logged in (have to do something with this)
-    # return render_template("error.html"), 404
     cursor = mysql.connection.cursor()
-    query = ''' SELECT netID, pw FROM classkonnect.users; '''
+    query = f''' SELECT netId, password FROM classkonnect.users WHERE netId = '{items['netid']}' AND password = '{items['pw']}'; '''
     cursor.execute(query)
-    users = cursor.fetchall()
-    for user in users:
-        if user[0] == items['netid'] and user[1] == items['pw']:
-            # successfully logged in
-            required_info = []
-            required_info.append(items["netid"])
-            # query2 = '''  '''
-            # cursor.execute()
-            cursor.close()
-            return render_template("main.html", items=required_info), 200
+    user = cursor.fetchall()
+    if len(user) == 1:
+        required_info = []
+        required_info.append(items["netid"])
+        query2 = f''' SELECT CRN FROM classkonnect.enrollments WHERE netId = '{items["netid"]}'; '''
+        cursor.execute(query2)
+        courses = cursor.fetchall()
+        enrollment_info = []
+        for course in courses:
+            query3 = f''' SELECT SubjectId, CourseNumber, SectionNumber FROM classkonnect.courses WHERE CRN = '{course[0]}'; '''
+            cursor.execute(query3)
+            section = cursor.fetchall()
+            enrollment_info.append(section)
+        required_info.append(enrollment_info)
+        # we're supposed to run a complex query here to obtain a list of similar students
+        #
+        #
+        cursor.close()
+        return render_template("main.html", items=required_info), 200
     # log-in failed
     return render_template("error.html"), 404
 
@@ -81,14 +68,35 @@ def user_info():
     """
     return render_template("base.html"), 200
 
-@app.route('/section/<course>/<section>', methods=["POST"])
-def section_info(course, section):
-    """Route for /section"""
-    # look up a section info
-    for sec in sections:
-        course_num = sec['subjectId'] + sec['courseNum']
-        if course_num == course and sec['sectionNum'] == section:
-            items = sec
-            return render_template("section.html", items=items), 200
-    # section info not found (have to do something with this)
-    return '', 404
+# @app.route('/section/<course>/<section>', methods=["POST"])
+# def section_info(course, section):
+#     """Route for /section"""
+#     # look up a section info
+#     # for sec in sections:
+#     #     course_num = sec['subjectId'] + sec['courseNum']
+#     #     if course_num == course and sec['sectionNum'] == section:
+#     #         items = sec
+#     #         return render_template("section.html", items=items), 200
+#     # section info not found (have to do something with this)
+#     return '', 404
+
+@app.route('/search/<user>', methods=["POST"])
+def search(user):
+    """Route for /search"""
+    # look up enrollments
+    cursor = mysql.connection.cursor()
+    query = f''' SELECT CRN FROM classkonnect.enrollments WHERE netId = '{user}'; '''
+    cursor.execute(query)
+    courses = cursor.fetchall()
+    required_info = []
+    for course in courses:
+        query2 = f''' SELECT SubjectId, CourseNumber, SectionNumber FROM classkonnect.courses WHERE CRN = '{course[0]}'; '''
+        cursor.execute(query2)
+        section = cursor.fetchall()
+        required_info.append(section)
+    cursor.close()
+    return render_template("search.html", items=required_info), 200
+
+@app.route('/search/<user>', methods=["POST"])
+def section_info(user):
+    """Route for /search"""
